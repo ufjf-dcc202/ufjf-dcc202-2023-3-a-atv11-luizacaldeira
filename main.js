348865cd161e574ddb1f148fa301fb41e31435ff
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-11-29 20:25:12 -0300

    a funçao atualizarItensDeLista foi criada

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
 olSaida.innerHTML = "";
+
+function atualizarItensDeLista(){
+    const lista = getLista();
+    for(let i = 0; i< lista.length ; i++){
+    const item = lista [i];
+    const li = document.createElement('li');
+    li.textContent = item;
+    }
+    }
+    
