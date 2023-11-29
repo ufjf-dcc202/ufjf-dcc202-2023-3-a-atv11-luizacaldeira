9c60d5d16f2501a661a403e724656069313180c7
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-11-29 20:23:57 -0300

    o arquivo main.js foi criado e adicionados conts

---
+++ main.js
@@ -0,0 +1,9 @@
+//main.js
+
+//Passo 1 - Ache os elementos importantes
+
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#saida');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+
