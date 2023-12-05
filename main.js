c0f2d104a778860110f198d6393e485fc474e989
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2023-12-05 14:39:45 -0300

    Ajustes finais para passar nos testes

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 //main.js
 
-import { adicionarNaLista, getLista, limparLista } from "./lista";
+import { adicionaNaLista, getLista, limpaLista, removeDaLista} from "./lista.js";
 
 //Passo 1 - Ache os elementos importantes
 
 const pEntrada = document.querySelector('#entrada');
-const olSaida = document.querySelector('#saida');
+const olSaida = document.querySelector('#itens');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-btnLimpar.addEventListener('click',limparElementosDaLista);
-btnAdicionar.addEventListener('click',criarElementosDaLista);
+btnLimpar.addEventListener('click', limparElementosDaLista);
+btnAdicionar.addEventListener('click', criaElementoNaLista);
 
 atualizarItensDeLista();
 
-function criarElementosNaLista(){
+function criaElementoNaLista(){
     const texto = pEntrada.textContent;
-    adicionarNaLista(texto);
+    adicionaNaLista(texto);
     atualizarItensDeLista();
     pEntrada.textContent="";
-    pEntrada.focus():
+    pEntrada.focus();
 }
 
 function limparElementosDaLista(){
-    limparLista();
+    limpaLista();
     atualizarItensDeLista();
 }
 
 function atualizarItensDeLista(){
     olSaida.innerHTML = "";
     const lista = getLista();
-    for(let i = 0; i< lista.length ; i++){
-    const item = lista [i];
-    const li = document.createElement('li');
-    li.textContent = item;
+    for(let i=0; i<lista.length;i++){
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
     }
 }
     
